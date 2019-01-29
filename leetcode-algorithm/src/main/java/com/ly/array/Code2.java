05b9c9db8ab42202e1c1ac1699eb0192dc861e5f
luoyao <luoyao@didichuxing.com>
2019-01-29 13:09:43 +0800

    买卖股票的最佳时机

--- leetcode-algorithm/src/main/java/com/ly/array/Code2.java
+++ leetcode-algorithm/src/main/java/com/ly/array/Code2.java
@@ -1,5 +1,43 @@
 package com.ly.array;
 
+/**
+ * 
+ * https://leetcode-cn.com/explore/featured/card/top-interview-questions-easy/1/array/22/
+ * @author ly
+ *
+ */
 public class Code2 {
-
+    
+    public static void main(String[] args) {
+        Code2 code2 = new Code2();
+        int[] arr = {7,6,4,3,1};
+        System.out.println(code2.maxProfit(arr));
+    }
+    
+    public int maxProfit(int[] prices) {
+    		if(prices == null || prices.length == 0) {
+    			return 0;
+    		}
+        int buy = Integer.MAX_VALUE;
+        int out = 0;
+        int sale = 0;
+        for(int i = 0 ; i < prices.length ; i++) {
+            //1. 找到买入点
+            if(buy > prices[i] && out == 0) {
+                buy = prices[i];
+            }else if (out < prices[i]) {
+                //2. 找到卖出点
+                out = prices[i];
+            }else {
+                //3. 卖出时机
+                sale += out - buy;
+                buy = prices[i];
+                out = 0;
+            }
+        }
+        if(out != 0) {
+        		sale += out - buy;
+        }
+        return sale;
+    }
 }
