cc394a2ed49ab51d494f1c3cd4daa6294811303c
luoyao <luoyao@didichuxing.com>
2019-01-30 21:47:07 +0800

    compe

---
+++ leetcode-algorithm/src/main/java/com/ly/competition/Code983.java
@@ -0,0 +1,16 @@
+package com.ly.competition;
+
+/**
+ * 
+ * https://leetcode-cn.com/contest/weekly-contest-121/problems/minimum-cost-for-tickets/
+ * @author ly
+ *
+ */
+public class Code983 {
+	
+    public int mincostTickets(int[] days, int[] costs) {
+    	    //int[] dp = new int[];
+    		return 0;
+    }
+    
+}
